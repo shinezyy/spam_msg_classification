d6dfc1146f90d79ef5d1df74cce14950b1905d79
Zhou Yaoyang <diamondzyy@sina.com>
2017-11-19 14:32:00 +0800

    chinese cut with jieba

---
+++ cut.py
@@ -0,0 +1,30 @@
+import jieba
+from os.path import join as pjoin
+from gensim.models import Word2Vec
+from pprint import pprint
+from array import array
+import numpy as np
+import pandas as pd
+import time
+
+
+start_time = time.time()
+df = pd.read_csv(pjoin('data', 'without-label.txt'), sep='\t',
+        header=None, na_filter=False, skip_blank_lines=False)
+print('Loading takes {}s'.format(time.time() - start_time))
+
+start_time = time.time()
+data = df.values
+sentences = []
+for row in data:
+    if row[-1] == '':
+        sentences.append(' ')
+    else:
+        sentences.append(' '.join(jieba.cut(row[-1].replace(' ', ''))))
+print('Cutting takes {}s'.format(time.time() - start_time))
+
+string_df = pd.DataFrame(sentences, columns=['contents'])
+string_df.to_csv(pjoin('data', 'cut-no-label.txt'),
+        header=None, index=None, sep='\t')
+
+
