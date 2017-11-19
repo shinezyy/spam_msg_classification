1bbe5b234ab2a75cf877684e8a05c754acfe079f
Zhou Yaoyang <diamondzyy@sina.com>
2017-11-19 15:50:07 +0800

    find the way to represent a sentence

--- to_vector.py
+++ to_vector.py
@@ -1,5 +1,6 @@
 from os.path import join as pjoin
-from gensim.models import Word2Vec
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import LabeledSentence
 from pprint import pprint
 import pandas as pd
 import time
@@ -8,8 +9,22 @@ import time
 start_time = time.time()
 df = pd.read_csv(pjoin('data', 'cut-labeled.txt'), sep='\t',
         header=None, na_filter=False, skip_blank_lines=False)
+matrix = df.values[:1000]
 print('Loading takes {}s'.format(time.time() - start_time))
 
 start_time = time.time()
-model = Word2Vec(df.values[:, 0], min_count=10, size=200)
-print('Word2Vector takes {}s'.format(time.time() - start_time))
+sentences = []
+for i in range(len(matrix)):
+    row = matrix[i]
+    sentence = LabeledSentence(row[0].split(' '), [str(i)])
+    sentences.append(sentence)
+
+model = Doc2Vec(alpha=0.025, min_alpha=0.025)  # use fixed learning rate
+model.build_vocab(sentences)
+model.train(sentences, total_examples=model.corpus_count, \
+        epochs=2,
+        )
+print('Doc2Vector takes {}s'.format(time.time() - start_time))
+
+print(matrix[0][0])
+print(model.infer_vector(matrix[0][0]))
