589fcfa8d869bac268c4f07223e9b6f94da2fa63
Zhou Yaoyang <diamondzyy@sina.com>
2017-11-20 08:57:30 +0800

    apply svm on a small dataset

---
+++ svm.py
@@ -0,0 +1,22 @@
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from os.path import join as pjoin
+import pandas as pd
+import numpy as np
+import time
+
+
+start_time = time.time()
+train_data_file = pjoin('data', 'vec-train-small.txt')
+df = pd.read_csv(train_data_file, sep=',', header=None)
+print('Loading takes {}s'.format(time.time() - start_time))
+matrix = df.values.astype(np.float32)
+
+X_train, X_test, y_train, y_test = train_test_split(
+        matrix[:, :-1], matrix[:, -1], test_size=0.2, random_state=0)
+
+start_time = time.time()
+clf = SVC(kernel='linear', C=1).fit(X_train, y_train)
+print('Training takes {}s'.format(time.time() - start_time))
+print(clf.score(X_test, y_test))
+
